ea9edaf69f4bbc8b2adc197637cff87845b164e6
GaoxiangYuan <gy549@nyu.edu>
2018-12-22 21:55:06 -0600

    first algorithm

--- src/rpc/RecommendPath.java
+++ src/rpc/RecommendPath.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RecommendPath
  */
-@WebServlet("/RecommendPath")
+@WebServlet("/path")
 public class RecommendPath extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
